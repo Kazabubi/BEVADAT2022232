d0d97a3f9860d0e1468e3898d8e9433a10ddc0b5
Kazabubi <venusz.adam@stud.uni-obuda.hu>
2023-04-03 16:51:44 +0200

    [HAZI05v1.0]

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn as sns
+import numpy as np
 from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix as conf_mat
@@ -40,3 +40,39 @@ class KNNClassifier:
         self.y_train = y_train
         self.x_test = x_test
         self.y_test = y_test
+
+    
+    def euclidean(self, element_of_x:pd.DataFrame) -> pd.DataFrame:
+        return (((self.x_train.loc[:,] - element_of_x.iloc[0])**2).sum(axis=1))**(1/2)
+    
+
+    def predict(self) -> None:
+        labels_pred = []
+        for x_test_element in self.x_test.iterrows():
+            distances = self.euclidean(pd.DataFrame(x_test_element[1]).transpose())
+            distances = pd.DataFrame(sorted(zip(distances,self.y_train)))
+            label_pred = mode(distances[1].head(self.k),keepdims=False).mode
+            labels_pred.append(label_pred)
+
+        self.y_preds = pd.Series(labels_pred, dtype = int)
+
+    
+    def accuracy(self) -> float:
+        true_positive = pd.DataFrame(zip(self.y_test, self.y_preds))
+        true_positive = true_positive[0] == true_positive[1]
+        return true_positive.sum() / len(self.y_test) * 100
+    
+
+    def confusion_matrix(self) -> np.ndarray:
+        conf_matrix = conf_mat(self.y_test,self.y_preds)
+        return conf_mat
+
+    def best_k(self) -> Tuple[int,float]:
+        acc = 0
+        r = 0
+        for k in range(1,21):
+            tmp = self.accuracy(y_test=self.y_test, y_preds=self.predict(self.x_train, self.y_train, self.x_test, k)) 
+            if(tmp> acc):
+                r = k
+                acc = tmp
+        return (r, acc)
